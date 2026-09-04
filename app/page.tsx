914fb3a813a17fe928bce4cd3afaf9f45fd5e5c3
Your Name <your.email@example.com>
2026-09-05 00:11:04 +0530

    updatess resume

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
       </a>
 
       <a
-        href="/RISHITA_RESUME word.docx"
+        href="/RISHITA_RESUME pdf.pdf"
         className="button secondary"
         target="_blank"
       >
