1052add69f264f42926c6609b33375cc795c80f9
Your Name <your.email@example.com>
2026-09-05 00:15:57 +0530

    upds resume

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
       </a>
 
       <a
-        href="/RISHITA_RESUME pdf.pdf"
+        href="/RISHITA_RESUME word.docx"
         className="button secondary"
         target="_blank"
       >
